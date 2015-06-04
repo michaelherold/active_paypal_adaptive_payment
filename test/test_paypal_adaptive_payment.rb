a4fb023ef4af169c26fed5d4afa006299955c910
Michael Herold <michael.j.herold@gmail.com>
2015-06-04 17:16:26 -0500

    Remove assert messages

--- test/test_paypal_adaptive_payment.rb
+++ test/test_paypal_adaptive_payment.rb
@@ -7,7 +7,7 @@ class TestPaypalAdaptivePayment < Minitest::Test
 
   def test_successful_pay
     assert response = @gateway.setup_purchase(fixtures(:pay_options))
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
     refute_nil response.authorization
   end
 
@@ -15,14 +15,14 @@ class TestPaypalAdaptivePayment < Minitest::Test
     assert response = @gateway.setup_purchase(fixtures(:pay_options))
     refute_nil key = response.authorization
     url = @gateway.redirect_url_for(key)
-    assert_match /#{key}$/, url, "Could not generate the proper redirect_url_for URL"
+    assert_match /#{key}$/, url
   end
 
   def test_redirect_url_for
     assert response = @gateway.setup_purchase(fixtures(:pay_options))
     refute_nil key = response.authorization
     url = @gateway.redirect_url_for(key)
-    assert_match /#{key}$/, url, "Could not generate the proper redirect_url_for URL"
+    assert_match /#{key}$/, url
   end
 
   def test_redirect_pre_approval_url_for
@@ -30,38 +30,38 @@ class TestPaypalAdaptivePayment < Minitest::Test
     assert response = @gateway.setup_purchase(fixtures(:pay_options))
     refute_nil key = response["preapprovalKey"]
     url = @gateway.redirect_pre_approval_url_for(key)
-    assert_match /#{key}$/, url, "Could not generate the proper redirect_url_for URL"
+    assert_match /#{key}$/, url
   end
 
   def test_embedded_flow_url_for
     assert response = @gateway.setup_purchase(fixtures(:pay_options))
     refute_nil key = response.authorization
     url = @gateway.embedded_flow_url_for(key)
-    assert_match /#{key}$/, url, "Could not generate the proper embedded_flow_url_for URL"
+    assert_match /#{key}$/, url
   end
 
   def test_successful_paydetails
     assert response = @gateway.details_for_payment(fixtures(:paydetails_options))
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
   end
 
   def test_successful_shipping_addresses
     assert response = @gateway.get_shipping_addresses(fixtures(:paydetails_options))
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
   end
 
   def test_successful_get_payment_options
     assert response = @gateway.get_payment_options(fixtures(:paydetails_options))
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
   end
 
   def test_successful_set_payment_options
     assert response = @gateway.set_payment_options(fixtures(:paydetails_options))
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
   end
 
   def test_successful_preapproval
     assert response = @gateway.preapprove_payment(preapproval_options)
-    assert_equal true, response.success?, "Unsuccessful Transaction"
+    assert_equal true, response.success?
   end
 end
