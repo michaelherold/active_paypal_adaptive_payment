0fe08ff18eaa1ab2404a83b689b386486d312e19
Michael Herold <michael.j.herold@gmail.com>
2015-06-04 15:29:00 -0500

    Clean up requires

--- lib/active_paypal_adaptive_payment.rb
+++ lib/active_paypal_adaptive_payment.rb
@@ -1,3 +1,2 @@
-# encoding: utf-8
 require 'active_merchant'
-require File.dirname(__FILE__) + '/active_merchant/billing/gateways/paypal_adaptive_payment'
+require 'active_merchant/billing/gateways/paypal_adaptive_payment'
