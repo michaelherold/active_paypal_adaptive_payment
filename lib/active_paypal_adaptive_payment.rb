0a172f1dc4643efa852ad95f2d4a1b605ab6454e
Michael Herold <michael.j.herold@gmail.com>
2015-06-04 15:26:48 -0500

    Remove integration
    ActiveMerchant deprecated integrations and moved them to their own
    `offsite_payments` gem in v1.44.0. As such, I'm removing the integration
    from this library. I might migrate the removed code over to another gem
    at a later date if it's something I think I'll need.

--- lib/active_paypal_adaptive_payment.rb
+++ lib/active_paypal_adaptive_payment.rb
@@ -1,5 +1,3 @@
 # encoding: utf-8
 require 'active_merchant'
 require File.dirname(__FILE__) + '/active_merchant/billing/gateways/paypal_adaptive_payment'
-require File.dirname(__FILE__) + '/active_merchant/billing/integrations/paypal_adaptive_payment'
-require File.dirname(__FILE__) + '/active_merchant/billing/integrations/notification'
